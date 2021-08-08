ac1d31d04df7ebe56d5de979fcbcff984ea650cd
Oleksandr Sovenko <info@oleksandrsovenko.com>
2021-08-08 02:25:46 +0000

    Work on GPIO (RIP-sensors)

--- app.js
+++ app.js
@@ -1,31 +1,98 @@
 // Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
-const fastifyStatic = require('fastify-static')
-const path = require('path')
-const { GPIO } = require('node-sandboxpi');
+const fastify       = require('fastify')({ logger: true }),
+      fastifyStatic = require('fastify-static'),
+      path          = require('path'),
+      { GPIO }      = require('node-sandboxpi');
 
+var relays = {
+   0: { pin: 3 , timer: null },
+   1: { pin: 12, timer: null },
+   2: { pin: 13, timer: null },
+   3: { pin: 14, timer: null },
+};
 
-GPIO.mode(30, GPIO.OUTPUT);
+var sensors = {
+   0: { pin: 15, },
+   1: { pin: 16 , },
+   2: { pin: 1, },
+   3: { pin: 11, },
+};
+
+leds = {
+   green:  { pin: 4 },
+   yellow: { pin: 5 }
+}
+
+// Initialize relays
+for (var index in relays) {
+   var relay = relays[index];
+
+   GPIO.mode(relay.pin, GPIO.OUTPUT);
+   GPIO.write(relay.pin, GPIO.HIGH);
+}
+
+// Fix
+// GPIO.mode(5, GPIO.OUTPUT);
+// GPIO.write(5, GPIO.LOW);
+
+// Initialize RIP-sensors
+for (var index in sensors) {
+   var sensor = sensors[index];
+
+   // console.log(sensor);
+
+   GPIO.mode(sensor.pin, GPIO.INPUT);
+}
+
+// Initialize LEDs
+// for (var index in leds) {
+//    var led = leds[index];
+
+//    GPIO.mode(led.pin, GPIO.OUTPUT);
+//    GPIO.write(led.pin, GPIO.LOW);
+// }
+
+// Blink LED Green
+// setInterval(function() {
+//    var led = GPIO.read(leds.green.pin);
+
+//    if (led === GPIO.HIGH)
+//       GPIO.write(leds.green.pin, GPIO.LOW);
+//    else
+//       GPIO.write(leds.green.pin, GPIO.HIGH);
+// }, 1000);
+
+// Buttons
+// GPIO.mode(16, GPIO.INPUT); // Reset
+
+// Main timer
 setInterval(function() {
-   var state = GPIO.read(30);
- 
-   if (state === GPIO.HIGH)
-      GPIO.write(30, GPIO.LOW);
-   else
-      GPIO.write(30, GPIO.HIGH);
-}, 2000);
+   // state16 = GPIO.read(16);
 
-GPIO.mode(7, GPIO.INPUT);
-setInterval(function() {
-   var state = GPIO.read(7);
- 
-   //if (state === GPIO.HIGH)
-   //   GPIO.write(30, GPIO.LOW);
-   //else
-   //   GPIO.write(30, GPIO.HIGH);
-   console.log(`GPIO7: ${state}`);
+   // if (state16 === GPIO.HIGH)
+   //    GPIO.write(15, GPIO.HIGH);
+   // else
+   //    GPIO.write(15, GPIO.LOW);
+
+   // Detect motion
+   for (var index in sensors) {
+      var sensor = sensors[index],
+          state  = GPIO.read(sensor.pin);
+
+      console.log(`sensor(${sensor.pin}): ${state}`);
+
+      if (state == GPIO.HIGH && relays[index].timer === null) {
+         GPIO.write(relays[index].pin, GPIO.LOW); // Relay On
+
+         relays[index].timer = setTimeout(function(index) {
+            relays[index].timer = null;
+            GPIO.write(relays[index].pin, GPIO.HIGH); // Relay Off
+         }, 10 * 1000, index);
+      }
+   }
+
+   console.log(``);
 }, 1000);
-
 
 
 fastify.register(require('fastify-static'), {
@@ -47,4 +114,5 @@ const start = async () => {
     process.exit(1)
   }
 }
+
 start()
---
+++ app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [ 1 ]
+do
+	node app.js
+	sleep 5
+done
